02d94a716139c91fed40e967d47286dda08f158c
Michael Niepel <michael.niepel@gmail.com>
2016-05-27 13:12:46 +0200

    put back base structure

---
+++ src/components/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+// Since this component is simple and static, there's no parent container for it.
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Appolo React</h1>
+    </div>
+  );
+};
+
+export default HomePage;
